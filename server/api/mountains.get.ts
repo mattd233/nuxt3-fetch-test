3df1f58bd686b8bc88f6e663f4db41ed56ccc216
Matt <matthewsdeng@gmail.com>
2022-12-18 10:40:52 +1100

    Use example code

---
+++ server/api/mountains.get.ts
@@ -0,0 +1,8 @@
+export default defineEventHandler(async(event) => {
+  return [
+    { title: 'Mount Everest' },
+    { title: 'Mount Fuji' },
+    { title: 'Denali' },
+    { title: 'Kangchenjunga,' }
+  ]
+})
